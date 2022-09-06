b73135a3503cf99be7190fc659ddf27c7e3d392b
myflowpl <pedro.blaszczak@gmail.com>
2022-09-06 11:13:20 +0200

    controller

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, NotFoundException, Param, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { ContactsFindAllDto } from './contacts.dto';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ContactExceptionDto, ContactsCreateDto, ContactsFindAllDto, ContactsUpdateDto } from './contacts.dto';
 import { Contact } from './contacts.entity';
 
 
-const data = [
+let data = [
   {id: 1, name: 'Piotr'},
   {id: 2, name: 'Paweł'},
 ]
@@ -21,6 +21,8 @@ export class ContactsController {
     return data.filter(contact => contact.id === parseInt(query.id, 10) || contact.name === query.name)
   }
 
+  @ApiResponse({status: 404, type: ContactExceptionDto, description: 'Contact not found' })
+  @ApiResponse({status: 500, type: ContactExceptionDto, description: 'Internal server error' })
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<Contact> {
 
@@ -33,9 +35,52 @@ export class ContactsController {
     return contact;
   }
 
-  async create() {}
+  @Post()
+  async create(@Body() contactDto: ContactsCreateDto): Promise<Contact> {
 
-  async update() {}
+    const contact: Contact = {
+      ...contactDto,
+      id: data.length +1,
+    }
 
-  async remove() {}
+    data.push(contact);
+
+    return contact;
+  }
+
+  @Patch(':id')
+  @HttpCode(204)
+  async update(@Body() contactDto: ContactsUpdateDto, @Param('id') id: string): Promise<Contact> {
+    
+    const contact = data.find(contact => contact.id === parseInt(id));
+
+    if(!contact) {
+      throw new NotFoundException(`Contact for id "${id}" was not found`);
+    }
+
+    const updatedContact: Contact = {
+      ...contact,
+      ...contactDto,
+    };
+
+    const index = data.findIndex(c => c === contact);
+
+    data[index] = updatedContact;
+
+    return updatedContact;
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<number> {
+
+    const index = data.findIndex(contact => contact.id === parseInt(id));
+
+    if(index < 0) {
+      throw new NotFoundException(`Contact for id "${id}" was not found`);
+    }
+
+    data = data.filter(c => c.id !== parseInt(id))
+
+    return parseInt(id);
+  }
 }
--- src/contacts/contacts.dto.ts
+++ src/contacts/contacts.dto.ts
@@ -1,5 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
 
 export class ContactsFindAllDto {
   name?: string
   id?: string
+}
+
+export class ContactExceptionDto {
+  statusCode: number;
+  message: string;
+  error: string;
+}
+
+export class ContactsCreateDto {
+  @ApiProperty({example: 'Iwona'})
+  name: string;
+
+  @ApiProperty({example: 'iwona@myflow.pl'})
+  email: string;
+}
+
+export class ContactsUpdateDto {
+  @ApiProperty({example: 'Julia'})
+  name: string;
 }
