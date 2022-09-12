ed7112d8d9f4d3df552332f3293b8579bcbb9368
myflowpl <pedro.blaszczak@gmail.com>
2022-09-12 11:56:55 +0200

    przykład dekoratorw w ts

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,43 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+// function Log(label) {
+//   return (target, key, descriptor) => {
+//     console.log(`LOG ${label}: ${key} was decorated!`, label, target, key, descriptor);
+
+//     const orgFn = descriptor.value;
+
+//     descriptor.value = function(...args) {
+//       console.log(` ${key} function logic replaced`)
+
+//       const data = orgFn.call(this, ...args)
+
+//       console.log(` ${key} function logic wrapped after`, data)
+
+//       return data;
+//     }
+//   };
+// }
+
+// function controller(...args) {
+//   console.log('CLASS DECORATOR', args)
+// }
+
+// @controller
+// class P {
+//   @Log('debug')
+//   foo() {
+//     console.log('foo() WORK IS RUNNING')
+//     return 'test data'
+//   }
+
+//   @Log('wargning')
+//   bar() {}
+// }
+
+// console.log('RUN THE APP')
+
+// const p = new P()
+
+// p.foo();
