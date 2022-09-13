cc371e6f0d59997288e1b9c71ad741acd2c19184
myflowpl <pedro.blaszczak@gmail.com>
2022-09-13 16:18:10 +0200

    on close intercpetor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { HttpService } from '@nestjs/axios';
-import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query, Render, UseInterceptors } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { delay, of, tap } from 'rxjs';
+import { catchError, combineLatest, delay, map, Observable, of, tap } from 'rxjs';
 import { AppService } from './app.service';
 import { ConfigService } from './config';
 import { StoreService } from './store/store.service';
+import { OnCloseInterceptor } from './users/interceptors/on-close.interceptor';
+import Axios from 'axios';
 
 @Controller()
 @ApiTags('App')
@@ -25,6 +27,7 @@ export class AppController {
   }
 
   @Get('autocomplete')
+  @UseInterceptors(OnCloseInterceptor)
   autocomplete(@Query('delay', ParseIntPipe) delayTime: number) {
 
     const data = ['test data', delayTime];
@@ -38,20 +41,62 @@ export class AppController {
   }
 
   @Get('big-data')
+  @UseInterceptors(OnCloseInterceptor)
+  // @Render('view/photos/list')
   bigData() {
 
     console.log('BIG DATA START')
 
-    const request5$ = this.request('http://localhost:3000/autocomplete?delay=5');
-    const request3$ = this.request('http://localhost:3000/autocomplete?delay=3');
-    const request1$ = this.request('http://localhost:3000/autocomplete?delay=1');
+    const request5$ = this.request('http://localhost:3000/autocomplete?delay=5').pipe(
+      catchError(err => of([]))
+    );
 
-    
+    const request3$ = this.request('http://localhost:3000/autocomplete?delay=3').pipe(
+      catchError(err => of([]))
+    );
 
+    const request1$ = this.request('http://localhost:3000/autocomplete?delay=1').pipe(
+      catchError(err => of([]))
+    );
+
+    const data$ = combineLatest([request5$, request3$, request1$]).pipe(
+      map(([data5, data3, data1]) => {
+        return [...data1, ...data5, ...data3];
+      }),
+      catchError(err => of(['fake data'])),
+      tap(() => console.log('BIG DATA END'))
+    )
+
+    return data$;
   }
 
   request(url: string) {
 
-    return this.http.get(url, {responseType: 'json'});
+    // WORK AROUND bo nie działa unsubscribe na oficjalnym serwisie
+    return new Observable<any[]>(subscriber => {
+      
+      const cancelSource = Axios.CancelToken.source();
+
+      const axios = this.http.axiosRef;
+
+
+      axios.get(url, {
+        cancelToken: cancelSource.token,
+      }).then(function(response) {
+         subscriber.next(response.data);
+         subscriber.complete();
+      }).catch(err => {
+        subscriber.error(err);
+      });
+
+      // cancel the request
+      return () => {
+        cancelSource.cancel();
+      }
+    });
+
+    return this.http.get<any[]>(url).pipe(
+      map(res => res.data),
+    );
   }
 }
--- src/users/interceptors/on-close.interceptor.ts
+++ src/users/interceptors/on-close.interceptor.ts
@@ -1,60 +1,70 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable, share, switchMap } from 'rxjs';
+import { endWith, fromEvent, Observable, share, switchMap, take, takeUntil, tap } from 'rxjs';
 
 @Injectable()
 export class OnCloseInterceptor implements NestInterceptor {
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+
+    const request = context.switchToHttp().getRequest();
+
+    const close$ = fromEvent(request, 'close');
+
+    return next.handle().pipe(
+      takeUntil(close$),
+      endWith(''),
+      take(1),
+      // tap((res) => console.log('ON CLOSE END', request.url))
+    );
   }
 
 }
 
 
-const p = new Promise((resolve, reject) => {
-  // TODO async job
-  // succes resolve(data)
-  // error  reject(error)
+// const p = new Promise((resolve, reject) => {
+//   // TODO async job
+//   // succes resolve(data)
+//   // error  reject(error)
 
-  // brak anulowania
-  // brak anulowania od strony fabryki (odpowiednika complete w observable)
-  // zwraca tylko jdedna wartosc
-})
+//   // brak anulowania
+//   // brak anulowania od strony fabryki (odpowiednika complete w observable)
+//   // zwraca tylko jdedna wartosc
+// })
 
-p.then(() => new Promise(() => {}))
-p.then(() => {})
-p.catch(() => { })
+// p.then(() => new Promise(() => {}))
+// p.then(() => {})
+// p.catch(() => { })
 
-const cold = new Observable((subscriber) => {
-  // CONSTRUCT
-  // TODO async job
-  subscriber.next({data: ''})
-  subscriber.error(new Error())
-  subscriber.complete()
+// const cold = new Observable((subscriber) => {
+//   // CONSTRUCT
+//   // TODO async job
+//   subscriber.next({data: ''})
+//   subscriber.error(new Error())
+//   subscriber.complete()
 
-  return () => {
-    // DESTRUCT
-  }
-})
+//   return () => {
+//     // DESTRUCT
+//   }
+// })
 
-const hot = cold.pipe(
-  share()
-)
+// const hot = cold.pipe(
+//   share()
+// )
 
-cold.pipe(
-  switchMap(() => hot)
-).subscribe()
+// cold.pipe(
+//   switchMap(() => hot)
+// ).subscribe()
 
-const subscription = cold.subscribe(
-  (value) => {},
-  (err) => {},
-  () => {},
-)
+// const subscription = cold.subscribe(
+//   (value) => {},
+//   (err) => {},
+//   () => {},
+// )
 
-subscription.unsubscribe()
+// subscription.unsubscribe()
 
-cold.subscribe(
-  (value) => {},
-  (err) => {},
-  () => {},
-)
+// cold.subscribe(
+//   (value) => {},
+//   (err) => {},
+//   () => {},
+// )
