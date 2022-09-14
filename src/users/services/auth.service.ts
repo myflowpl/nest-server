1edeb007102e2ff4d632ecd4fda4c8a407ad0b6f
myflowpl <pedro.blaszczak@gmail.com>
2022-09-14 11:22:13 +0200

    auth service

--- src/users/services/auth.service.ts
+++ src/users/services/auth.service.ts
@@ -1,19 +1,40 @@
 import { Injectable } from '@nestjs/common';
-// import { JwtService } from '@nestjs/jwt';
-import { RequestPayload } from '../entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { RequestPayload, TokenPayload, User } from '../entities/user.entity';
 import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  // constructor(private usersService: UsersService, private jwtService: JwtService) {}
+
+  constructor(
+    private usersService: UsersService, 
+    private jwtService: JwtService
+  ) {}
 
   // async validateUser(email: string, password: string): Promise<User | null> {}
 
-  // async encodePassword(password: string): Promise<string> {}
+  async encodePassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
+  }
 
-  // async validatePassword(password: string, hash: string): Promise<boolean> {}
+  async validatePassword(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
 
-  // async encodeUserToken(user: User): Promise<string> {}
+  async encodeUserToken(user: User): Promise<string> {
+    
+    const payload: TokenPayload = { sub: user.id }
 
-  // async decodeUserToken(token: string): Promise<RequestPayload> {}
+    return this.jwtService.signAsync(payload);
+  }
+
+  async decodeUserToken(token: string): Promise<RequestPayload> {
+
+    const payload: TokenPayload = await this.jwtService.verifyAsync(token);
+
+    const user = await this.usersService.findOne(payload.sub);
+
+    return user ? { user } : null;
+  }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,22 @@ import { AuthController } from './controllers/auth.controller';
 import { UsersService } from './services/users.service';
 import { AuthService } from './services/auth.service';
 import { StoreModule } from '../store/store.module';
-import { ConfigModule } from '../config';
+import { ConfigModule, ConfigService } from '../config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [StoreModule, ConfigModule],
+  imports: [
+    StoreModule, 
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.JWT_SECRET,
+        signOptions: { expiresIn: '4d'}
+      })
+    })
+  ],
   controllers: [AuthController],
   providers: [UsersService, AuthService],
   exports: [UsersService, AuthService],
