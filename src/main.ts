376d8e729899c6e757b66c02e470db39be1b9c23
myflowpl <pedro.blaszczak@gmail.com>
2022-11-07 09:49:20 +0100

    decorators explained

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,42 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+// console.log('DECORATORY :)')
+
+
+// function Log(label) {
+//   return (target, key, descriptor) => {
+
+//     console.log(`LOG ${label}: ${key} was decorated!`, label, target, key);
+ 
+//     const orgFn = descriptor.value;
+ 
+//     descriptor.value = function(...args) {
+//       console.log(` ${key} function logic replaced`)
+ 
+//       const data = orgFn.call(this, ...args)
+ 
+//       console.log(` ${key} function logic wrapped after`, data)
+ 
+//       return data;
+//     }
+//   };
+// }
+ 
+// class P {
+//   @Log("Warn")
+//   @Log("debug")
+//   foo() { 
+//     console.log('FOO ORGINAL FUNCTION')
+//     return 'hej hej'
+//    }
+ 
+//   @Log("warning")
+//   bar() {  }
+// }
+// // foo was decorated!
+ 
+// const p = new P();
+// p.foo();
+// // Do something
