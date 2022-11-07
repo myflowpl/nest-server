29723c688c015643fcc49f5414cd50668d5f88ec
myflowpl <pedro.blaszczak@gmail.com>
2022-11-07 12:43:12 +0100

    contact controllers

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { StoreService } from '../store/store.service';
+import { CreateContactDto, ErrorResponse, GetContactsDto, UpdateContactDto } from './contacts.dto';
 import { Contact } from './contacts.entity';
 
 @Controller('contacts')
 @ApiTags('Contacts')
+@ApiResponse({status: 500, type: ErrorResponse, description: 'Internal Server Error'})
 export class ContactsController {
 
   constructor(
@@ -12,10 +14,66 @@ export class ContactsController {
   ) {}
 
   @Get()
-  async findAll(): Promise<Contact[]> {
+  async findAll(@Query() query: GetContactsDto): Promise<Contact[]> {
 
     const contacts = await this.store.findAll(Contact)
 
-    return contacts;
+    return contacts.slice(
+      (query.page-1)*query.pageSize,
+      query.page * query.pageSize
+    );
+  }
+
+  @Post()
+  async create(@Body() data: CreateContactDto): Promise<Contact> {
+
+    const existingEmail = await this.store.findAll(Contact).then(
+      contacts => contacts.find(c => c.email === data.email)
+    )
+
+    if(existingEmail) {
+      throw new BadRequestException(`Email ${data.email} is already taken`)
+    }
+
+    const contact = await this.store.create(Contact, data);
+
+    return contact;
+  }
+
+  @Get(':id')
+  @ApiResponse({status: 404, type: ErrorResponse, description: 'Contact not found'})
+  async findOne(@Param('id') id: string) {
+
+    const contact = await this.store.findOne(Contact, +id);
+
+    if(!contact) {
+      throw new NotFoundException(`Contact for id ${id} not found`);
+    }
+
+    return contact
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() data: UpdateContactDto) {
+
+    const contact = await this.store.update(Contact, +id, data);
+
+    if(!contact) {
+      throw new NotFoundException(`Contact for id ${id} not found`);
+    }
+
+    return contact;
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+
+    const contact = await this.store.remove(Contact, +id);
+    
+    if(!contact) {
+      throw new NotFoundException(`Contact for id ${id} not found`);
+    }
+
+    return contact;
   }
 }
--- src/contacts/contacts.dto.ts
+++ src/contacts/contacts.dto.ts
@@ -1,1 +1,30 @@
-export class ContactsDto {}
+
+export class CreateContactDto {
+  name: string;
+  email: string;
+  message?: string;
+}
+
+export enum SortDir {
+  ASC = 'asc',
+  DESC = 'desc',
+}
+
+export class GetContactsDto {
+  page?: number;
+  pageSize?: number;
+
+  sortBy?: string;
+  sortDir?: SortDir
+}
+
+export class ErrorResponse {
+  statusCode: number;
+  message: string;
+  error: string;
+}
+
+export class UpdateContactDto {
+  name: string;
+  message: string;
+}
