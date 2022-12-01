fee9a4e95afb468b9161a7f6cf017e4985980fe1
myflowpl <pedro.blaszczak@gmail.com>
2022-12-01 16:18:59 +0100

    on close interceptor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query, Req, UseInterceptors } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import { combineLatest, delay, endWith, of, Subject, take, takeUntil, tap } from 'rxjs';
 import { AppService } from './app.service';
+import { OnCloseInterceptor } from './users/interceptors/on-close.interceptor';
 
 @Controller()
 @ApiTags('App')
@@ -16,5 +19,45 @@ export class AppController {
     const res = await this.appService.getHello();
 
     return res;
+  }
+
+  @Get('microservice')
+  microservice(
+    @Query('delay', ParseIntPipe) delayTime: number,
+    @Req() request: Request,
+  ) {
+
+    const close$ = new Subject();
+
+    request.on('close', () => {
+      console.log('MICRO Cancel', delayTime)
+      close$.next(true);
+    })
+
+    console.log('MICRO Start', delayTime)
+
+    return of(`Response FROM ${delayTime}`).pipe(
+      delay(delayTime*1000),
+      takeUntil(close$),
+      tap({
+        next: () => console.log('MICRO End', delayTime),
+        finalize: () => console.log('MICRO FINAL', delayTime),
+      }),
+      endWith(''),
+      take(1)
+    )
+  }
+
+  @Get('search')
+  @UseInterceptors(OnCloseInterceptor)
+  search() {
+
+    const req5$ = this.appService.request(`http://localhost:3000/microservice?delay=5`);
+    const req3$ = this.appService.request(`http://localhost:3000/microservice?delay=3`);
+    const req1$ = this.appService.request(`http://localhost:3000/microservice?delay=1`);
+
+    const data$ = combineLatest([req5$, req3$, req1$])
+    
+    return data$;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { StoreService } from './store/store.service';
+import Axios from 'axios';
+import { Observable } from 'rxjs';
+
 
 @Injectable()
 export class AppService {
@@ -11,6 +14,30 @@ export class AppService {
 
   async getHello(): Promise<any> {
     return this.store.data;
+  }
+
+  request(url: string) {
+
+    return new Observable(subscriber => {
+      // constructor
+
+      const controller = new AbortController();
+  
+      Axios.get(url, {signal: controller.signal})
+        .then(res => {
+          subscriber.next(res.data);
+          subscriber.complete();
+        })
+        .catch(err => {
+          subscriber.error(err);
+        });
+
+      return () => {
+        // descructor
+        controller.abort();
+      }
+    });
+
   }
 }
  
