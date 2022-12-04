276d7296003e2b6fe6f35ffd365e3109fe4eabaa
myflowpl <pedro.blaszczak@gmail.com>
2022-12-04 16:02:43 +0100

    unit tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -3,12 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
+
   let appController: AppController;
+  const message = 'Hello from Nest !!';
 
   beforeEach(async () => {
+
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [{
+        provide: AppService,
+        useValue: {
+          async getHello(): Promise<any> {
+            return message
+          }
+        }
+      }],
     }).compile();
 
     appController = app.get<AppController>(AppController);
@@ -16,7 +27,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).resolves.toBe(message);
     });
   });
 });
