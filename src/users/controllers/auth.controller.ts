18e81901d271aaeb1103006fce8ba58afd18657d
myflowpl <pedro.blaszczak@gmail.com>
2022-12-08 15:10:30 +0100

    decorator composition

--- src/users/controllers/auth.controller.ts
+++ src/users/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiAuth } from '../decorators/api-auth.decorator';
 import { Auth } from '../decorators/auth.decorator';
 import { Roles } from '../decorators/roles.decorator';
 import { ExceptionResponse, RoleNames, User } from '../entities/user.entity/user.entity';
@@ -10,11 +11,7 @@ import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 export class AuthController {
 
   @Get()
-  @UseGuards(JwtAuthGuard)
-  @ApiBearerAuth()
-  @Roles(RoleNames.ROOT)
-  @ApiResponse({status: 401, type: ExceptionResponse, description: 'Unauthorized, JWT token required'})
-  @ApiResponse({status: 403, type: ExceptionResponse, description: 'Unauthenticated, some roles required'})
+  @ApiAuth(RoleNames.ROOT)
   me(@Auth() user: User) {
 
     // TODO do some logic for the authenticated user
--- src/users/decorators/api-auth.decorator.ts
+++ src/users/decorators/api-auth.decorator.ts
@@ -1,1 +1,13 @@
+import { applyDecorators, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiResponse } from "@nestjs/swagger";
+import { RoleNames, ExceptionResponse } from "../entities/user.entity/user.entity";
+import { JwtAuthGuard } from "../guards/jwt-auth.guard";
+import { Roles } from "./roles.decorator";
 
+export const ApiAuth = (...roles: RoleNames[]) => applyDecorators(
+  UseGuards(JwtAuthGuard),
+  ApiBearerAuth(),
+  Roles(...roles),
+  ApiResponse({status: 401, type: ExceptionResponse, description: 'Unauthorized, JWT token required'}),
+  ApiResponse({status: 403, type: ExceptionResponse, description: 'Unauthenticated, some roles required'}),
+);
