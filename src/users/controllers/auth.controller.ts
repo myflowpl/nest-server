f233347008604a5ebf0cc5418de016f6ac1ca9a7
myflowpl <pedro.blaszczak@gmail.com>
2023-07-18 10:57:52 +0200

    register

--- src/users/controllers/auth.controller.ts
+++ src/users/controllers/auth.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { RoleNames, User } from '../entities/user.entity';
 import { Auth } from '../decorators/auth.decorator';
 import { ApiAuth } from '../decorators/api-auth.decorator';
+import { AuthRegisterDto } from '../dto/auth.dto';
+import { AuthService } from '../services/auth.service';
+import { UsersService } from '../services/users.service';
 
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
+
+    constructor(
+        private authService: AuthService,
+        private usersService: UsersService,
+    ) {}
 
     @Get()
     @ApiAuth(RoleNames.ROOT)
@@ -14,5 +22,26 @@ export class AuthController {
     me(@Auth() user: User, @Auth('token') token: string ) {
         
         return { user, token };
+    }
+
+    @Post('register')
+    async register(@Body() data: AuthRegisterDto) {
+
+        let user = await this.usersService.findOneBy({ email: data.email });
+
+        if(user) {
+            throw new BadRequestException(`Email ${data.email} is already taken`)
+        }
+
+        const password = await this.authService.encodePassword(data.password);
+
+        user = new User({
+            ...data,
+            password,
+        });
+
+        await this.usersService.save(user);
+
+        return user;
     }
 }
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { StoreService } from '../../store/store.service';
+import { User } from '../entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(
+        private store: StoreService,
+    ) {}
+
+    async findOneBy(query: Partial<User>) {
+        return this.store.findOneBy(User, query)
+    }
+
+    async save(user: User) {
+
+        return this.store.save(user);
+
+    }
+}
