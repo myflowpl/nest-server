a0994ea42dd61547a16437b5b396f8d025095d13
myflowpl <pedro.blaszczak@gmail.com>
2025-10-28 15:07:34 +0100

    modules config & store

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger';
+import { StoreService } from './store/store.service';
+import { ConfigService } from './config';
 
 @ApiTags('App')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+
+  constructor(
+    private readonly appService: AppService,
+    private readonly storeService: StoreService,
+    private readonly config: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -14,6 +21,7 @@ export class AppController {
 
   @Get('name')
   name(): string {
+    // console.log('FROM STORE', this.storeService.findOneBy())
     return 'test';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StoreModule } from './store/store.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [StoreModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [],
 })
 export class AppModule {}
