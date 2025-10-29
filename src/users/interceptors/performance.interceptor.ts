e3e87aa8758bcf0d55cd8051f9b4077f19488008
myflowpl <pedro.blaszczak@gmail.com>
2025-10-29 15:59:20 +0100

    interceptors

---
+++ src/users/interceptors/performance.interceptor.ts
@@ -0,0 +1,47 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Request } from 'express';
+import { catchError, map, Observable, of, tap } from 'rxjs';
+
+@Injectable()
+export class PerformanceInterceptor implements NestInterceptor {
+
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+
+    const request: Request = context.switchToHttp().getRequest();
+
+    // BEFORE hook, modify the request
+
+    // REPLACE controller
+    if(!request.payload) {
+      return of({messag: 'my response from interceptor'})
+    }
+
+    const next$ = next.handle();
+
+    // subscirbne to the streem
+    // next$.subscribe({
+    //   next: (value) => console.log('next', value)
+    // })
+
+    // run controller or next interceptor
+    const response$ = next$.pipe(
+
+      // AFTER hook map the response
+      map(response => response),
+
+      // intercept error response
+      catchError((err) => of(err)),
+
+      // watch te response
+      tap({
+        finalize: () => {
+          console.log('requesty is DONE')
+        }
+      })
+    );
+
+    return response$;
+  }
+
+}
