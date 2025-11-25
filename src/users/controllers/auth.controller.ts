b1ae54b968aa29784c30a378b366be7e4d494dd9
myflowpl <pedro.blaszczak@gmail.com>
2025-11-25 09:47:35 +0100

    jwt auth guard

--- src/users/controllers/auth.controller.ts
+++ src/users/controllers/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
-
+    
     @Get()
     @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
     me() {
 
         return 'working'
--- src/users/guards/jwt-auth.guard.ts
+++ src/users/guards/jwt-auth.guard.ts
@@ -1,11 +1,36 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-  canActivate( context: ExecutionContext ): boolean | Promise<boolean> | Observable<boolean> {
 
-    return false;
+  async canActivate( context: ExecutionContext ): Promise<boolean> {
+
+    let token = '';
+
+    if(context.getType() === 'http') {
+      // get request from context
+      const request: Request = context.switchToHttp().getRequest();
+  
+      // extract token from request
+      token = request.headers.authorization?.replace('Bearer ', '') || '';
+
+    } else if(context.getType() === 'ws') {
+      // TODO
+      const data = context.switchToWs().getData();
+      throw new InternalServerErrorException(`not suppported type of context "${context.getType()}"`);
+      
+    } else if(context.getType() === 'rpc') {
+      // TODO implement other conext rpc 
+      throw new InternalServerErrorException(`not suppported type of context "${context.getType()}"`);
+    }
+
+    // validate token if exists
+    if(!token) {
+      return false;
+    }
+
+    return true;
     
   }
 }
