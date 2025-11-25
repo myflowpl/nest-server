df45e4b8083a08c903dc3d01e25023d19291fa66
myflowpl <pedro.blaszczak@gmail.com>
2025-11-25 10:09:48 +0100

    guard is working

--- src/users/guards/jwt-auth.guard.ts
+++ src/users/guards/jwt-auth.guard.ts
@@ -1,19 +1,37 @@
-import { CanActivate, ExecutionContext, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { Request } from 'express';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
 
+  constructor(
+    private authService: AuthService,
+  ){}
+
   async canActivate( context: ExecutionContext ): Promise<boolean> {
 
     let token = '';
+    let request: Request;
 
     if(context.getType() === 'http') {
       // get request from context
-      const request: Request = context.switchToHttp().getRequest();
+      request = context.switchToHttp().getRequest();
   
       // extract token from request
       token = request.headers.authorization?.replace('Bearer ', '') || '';
+
+      // validate token if exists
+      if(!token) {
+        throw new UnauthorizedException(`JWT required in Authorization header`)
+      }
+
+      // validate & decode token to TokenPayload
+      request.payload = await this.authService.decodeUserToken(token);
+
+      console.log('request payload', request.payload)
+
+      return true;
 
     } else if(context.getType() === 'ws') {
       // TODO
@@ -24,13 +42,8 @@ export class JwtAuthGuard implements CanActivate {
       // TODO implement other conext rpc 
       throw new InternalServerErrorException(`not suppported type of context "${context.getType()}"`);
     }
-
-    // validate token if exists
-    if(!token) {
-      return false;
-    }
-
-    return true;
+    
+    throw new InternalServerErrorException(`not suppported type of context "${context.getType()}"`);
     
   }
 }
--- src/users/services/auth.service.ts
+++ src/users/services/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { RequestPayload, RoleNames, User } from '../entities/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    async decodeUserToken(token: string): Promise<RequestPayload | null> {
+
+        // mock fake user
+        const user = new User({
+            id: 1,
+            name: 'Piotr',
+            roles: [{ id: 1, name: RoleNames.ROOT }]
+        });
+
+        return token ? { user } : null;
+    }
+}
