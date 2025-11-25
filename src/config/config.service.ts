ec34504fa2962ae54b605bf4a2ade5c1ed742b81
myflowpl <pedro.blaszczak@gmail.com>
2025-11-25 11:42:14 +0100

    config dotenv

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { resolve } from 'path';
+import 'dotenv/config'
 
 @Injectable()
 export class ConfigService {
 
-    readonly DEBUG = true;
-    readonly PORT = 3000;
+    readonly DEBUG = process.env.DEBUG === 'true';
+    readonly PORT = parseInt(process.env.PORT || '0', 10);
+    readonly DOMAIN = process.env.DOMAIN;
+    readonly STORAGE_DIR = resolve(process.env.STORAGE_DIR || '');
+    readonly JWT_SECRET = process.env.JWT_SECRET;
 
     async onModuleInit() {
         // console.log('init config async')
