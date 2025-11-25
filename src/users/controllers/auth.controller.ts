bcaf411f33bf2f0bbca40e86f9024bc2b64eb52b
myflowpl <pedro.blaszczak@gmail.com>
2025-11-25 13:02:21 +0100

    user register

--- src/users/controllers/auth.controller.ts
+++ src/users/controllers/auth.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, UseGuards, ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Auth } from '../decorators/auth.decorator';
 import { ExceptionResponse, RoleNames, User } from '../entities/user.entity';
 import { Roles } from '../decorators/roles.decorator';
 import { ApiAuth } from '../decorators/api-auth.decorator';
+import { AuthRegisterDto } from '../dto/auth.dto';
+import { UsersService } from '../services/users.service';
+import { AuthService } from '../services/auth.service';
 
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
+
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService,
+    ) {}
     
     @Get()
     @ApiAuth(RoleNames.ROOT)
@@ -17,4 +25,30 @@ export class AuthController {
         return [user, token];
     }
 
+    @Post('register')
+    async register(@Body(ValidationPipe) data: AuthRegisterDto) {
+
+        // validate input data with ValidationPipe & Decorators on DTO
+
+        // check if user exists
+        let user = await this.usersService.findOneBy({ email: data.email });
+        if(user) {
+            throw new BadRequestException(`Email "${data.email}" is already taken`)
+        }
+
+        // createh password hash
+        const password = await this.authService.encodePassword(data.password);
+
+        // create user entity
+        user = new User({
+            ...data,
+            password,
+        })
+
+        // save entity
+        await this.usersService.save(user);
+
+        // return user
+        return user;
+    }
 }
--- src/users/dto/auth.dto.ts
+++ src/users/dto/auth.dto.ts
@@ -1,1 +1,17 @@
-export class AuthDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, MinLength } from "class-validator";
+
+export class AuthRegisterDto {
+    @MinLength(3)
+    @ApiProperty({example: 'Piotr'})
+    name: string;
+    
+    @IsEmail()
+    @ApiProperty({example: 'piotr@myflow.pl'})
+    email: string;
+    
+    @MinLength(4)
+    @ApiProperty({example: '!@#$'})
+    password: string;
+}
+
