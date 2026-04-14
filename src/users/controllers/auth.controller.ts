edb607baa558de13da455b7d1396b79b018f0996
myflowpl <pedro.blaszczak@gmail.com>
2026-04-14 11:00:28 +0200

    register & login

--- src/users/controllers/auth.controller.ts
+++ src/users/controllers/auth.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { RoleNames, User } from '../entities/user.entity';
 import { Auth } from '../decorators/auth.decorator';
-import { MeResponse } from '../dto/auth.dto';
+import { AuthLoginDto, AuthLoginResponse, AuthRegisterDto, MeResponse } from '../dto/auth.dto';
 import { ApiAuth } from '../decorators/api-auth.decorator';
+import { StoreService } from '../../store/store.service';
+import { AuthService } from '../services/auth.service';
 
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
 
+    constructor(
+        private store: StoreService,
+        private authService: AuthService,
+    ){}
+
     @Get('me')
-    @ApiAuth(RoleNames.ADMIN)
+    @ApiAuth()
     me(
         @Auth() user: User,
         @Auth('token') token: string,
@@ -21,5 +28,55 @@ export class AuthController {
         return { user, token };
     }
 
+    @Post('register')
+    @UsePipes(new ValidationPipe({
+        transform: true,
+        transformOptions: { enableImplicitConversion: true }
+    }))
+    async register(@Body() data: AuthRegisterDto): Promise<User> {
+
+        let user = await this.store.findOneBy(User, { email: data.email });
+
+        if(user) {
+            throw new BadRequestException('user email is taken');
+        }
+
+        // chash password
+        const password = await this.authService.encodePassword(data.password);
+
+        // create entity
+        user = new User({
+            ...data,
+            password,
+        })
+
+        // save entity
+        await this.store.save(user);
+
+        return user;
+
+    }
+
+    @Post('login')
+    async login(@Body(ValidationPipe) data: AuthLoginDto): Promise<AuthLoginResponse> {
+
+
+        let user = await this.store.findOneBy(User, { email: data.email });
+
+        if(!user) {
+            throw new BadRequestException('Bad credentials');
+        }
+
+        const isValid = await this.authService.validatePassword(data.password, user.password);
+
+        if(!isValid) {
+            throw new BadRequestException('Bad credentials');
+        }
+
+        const token = await this.authService.encodeUserToken(user);
+
+        return { token, user }
+
+    }
 
 }
--- src/users/dto/auth.dto.ts
+++ src/users/dto/auth.dto.ts
@@ -1,6 +1,60 @@
+import { IsEmail, MinLength, registerDecorator, ValidationArguments, ValidationOptions } from "class-validator";
 import { User } from "../entities/user.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class MeResponse {
     user: User;
     token: string;
 }
+
+export class AuthRegisterDto {
+
+    @ApiProperty({ example: 'Piotr' })
+    @MinLength(3)
+    name: string;
+    
+    @ApiProperty({ example: 'piotr@myflow.pl' })
+    @IsEmail()
+    email: string;
+    
+    @ApiProperty({ example: '!@#$' })
+    @IsPassword()
+    password: string;
+}
+export class AuthLoginDto {
+    
+    @ApiProperty({ example: 'piotr@myflow.pl' })
+    @IsEmail()
+    email: string;
+    
+    @ApiProperty({ example: '!@#$' })
+    @IsPassword()
+    password: string;
+}
+
+export class AuthLoginResponse {
+  token: string;
+  user: User;
+}
+
+
+export function IsPassword(validationOptions?: ValidationOptions) {
+    return function (object: Object, propertyName: string) {
+      registerDecorator({
+        name: 'isPassword',
+        target: object.constructor,
+        propertyName: propertyName,
+        constraints: [],
+        options: {
+          message: 'Password has to be equal or grater then 4 characters',
+          ...validationOptions,
+        },
+        validator: {
+          validate(value: any, args: ValidationArguments) {
+            // TODO dostosowac validacje
+            return (''+value).length >=4;
+          },
+        },
+      });
+    };
+  }
